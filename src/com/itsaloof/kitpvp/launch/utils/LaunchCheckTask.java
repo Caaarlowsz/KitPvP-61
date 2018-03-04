c41a43e8d8934703714335a74b2bc41d1497f41e
SynchronousX <17569708+SynchronousX@users.noreply.github.com>
2018-03-04 11:17:43 -0500

    Fix slime block damage negation

--- src/com/itsaloof/kitpvp/launch/utils/LaunchCheckTask.java
+++ src/com/itsaloof/kitpvp/launch/utils/LaunchCheckTask.java
@@ -3,6 +3,7 @@ package com.itsaloof.kitpvp.launch.utils;
 import com.itsaloof.kitpvp.KitPvPPlugin;
 import com.itsaloof.kitpvp.launch.events.LaunchEvent;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -10,35 +11,65 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 public class LaunchCheckTask extends BukkitRunnable {
-    private static final List<Material> additionalGroundMaterials = Arrays.asList(
+    private static final List<Material> collisionMaterials = Arrays.asList(
             Material.LADDER,
             Material.LAVA,
-            Material.SLIME_BLOCK,
             Material.STATIONARY_LAVA,
             Material.STATIONARY_WATER,
             Material.VINE,
             Material.WATER
     );
     private final KitPvPPlugin plugin;
-    private final LaunchEvent event;
+    private final Player player;
+    private final Entity playerEntity;
+    private final UUID uuid;
+    private boolean bouncingLow = false;
 
-    public LaunchCheckTask(final KitPvPPlugin plugin, LaunchEvent event) {
+    public LaunchCheckTask(final KitPvPPlugin plugin, final LaunchEvent event) {
         this.plugin = plugin;
-        this.event = event;
+        this.player = event.getPlayer();
+        this.playerEntity = (Entity) this.player;
+        this.uuid = this.player.getUniqueId();
     }
 
     @Override
     public void run() {
-        final Player player = event.getPlayer();
-        if (!this.plugin.launchUtils.isOnLaunchpad(player)) {
-            final Material material = player.getLocation().getBlock().getRelative(BlockFace.DOWN).getType();
-            if (!(material == Material.SLIME_BLOCK && additionalGroundMaterials.contains(Material.SLIME_BLOCK) && !player.isSneaking()) && (((Entity) player).isOnGround() || additionalGroundMaterials.contains(material))) {
-                this.plugin.launchUtils.endLaunch(player);
-                this.cancel();
+        if (this.plugin.launchUtils.isBeingLaunched(this.uuid)) {
+            if (this.plugin.launchUtils.isOnLaunchpad(this.uuid)) {
+                return;
             }
+
+            final Block containingBlock = this.player.getLocation().getBlock();
+            final Material containingMaterial = containingBlock.getType();
+            final Material belowMaterial = containingBlock.getRelative(BlockFace.DOWN).getType();
+
+            if (belowMaterial == Material.SLIME_BLOCK) {
+                if (this.player.getVelocity().getY() > 0.0) {
+                    if (!this.bouncingLow) {
+                        this.bouncingLow = true;
+                    }
+
+                    return;
+                } else if (!this.bouncingLow) {
+                    return;
+                }
+            } else {
+                if (this.bouncingLow) {
+                    this.bouncingLow = false;
+                }
+
+                if (!this.playerEntity.isOnGround() && !collisionMaterials.contains(containingMaterial)) {
+                    return;
+                }
+            }
+
+            this.plugin.launchUtils.endLaunch(this.player, this.uuid);
         }
+
+        this.cancel();
     }
 
     public void begin() {
